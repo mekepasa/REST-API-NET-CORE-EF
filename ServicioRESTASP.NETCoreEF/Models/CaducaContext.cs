df7f2ab169f822a9cedf9f76a3634f4c172ab99b
javiergonzalezramos <mekepasa@hotmail.com>
2020-03-31 17:40:56 +0200

    Add slqServer context

---
+++ ServicioRESTASP.NETCoreEF/Models/CaducaContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServicioRESTASP.NETCoreEF.Models
+{
+    public class CaducaContext : DbContext
+    {
+        public CaducaContext(DbContextOptions<CaducaContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categoria { get; set; }
+    }
+}
---
+++ ServicioRESTASP.NETCoreEF/Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServicioRESTASP.NETCoreEF.Models
+{
+    public class Categoria
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor teclea el campo Clave")]
+        [Range(1, 999)]
+        public int Clave { get; set; }
+
+        [StringLength(80)]
+        [Required(ErrorMessage = "Por favor teclea el campo Nombre")]
+        [Column(TypeName = "VARCHAR(80)")]
+        public string Nombre { get; set; }
+    }
+}
